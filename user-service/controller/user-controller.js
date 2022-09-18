785170ac733eac28b21cd2077bf8657fccc7f258
goatygoatygoat <52666150+goatygoatygoat@users.noreply.github.com>
2022-09-18 16:59:22 +0800

    Hash and check password, change password

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,5 +1,7 @@
 import { ormCheckUser as _checkUser } from '../model/user-orm.js'
 import { ormCreateUser as _createUser } from '../model/user-orm.js'
+import { ormCheckPassword as _checkPassword } from '../model/user-orm.js'
+import { ormChangePassword as _changePassword } from '../model/user-orm.js'
 import jwt from 'jsonwebtoken'
 
 export async function createUser(req, res) {
@@ -30,18 +32,27 @@ export async function userLogin(req, res) {
     try {
         const { username, password } = req.body;
         const user = await _checkUser(username);
-    
-        if (!user) {
-            return res.status(404).json({message: `Username: ${username} not found in database!`})
-        }
-        // todo: add check to check if password is correct and return 401/403 (need check) if incorrect password
-        // todo: add check to check if there are any fields left blank, return 500 if so
 
-        const token = jwt.sign({
-            username: username
-        }, "helloworld", { expiresIn: '3h'}) // helloworld is the jwt secret key, it's just an example and should put in env file
-        
-        return res.status(200).json({message: 'Authentication successful', token: token})
+        // todo: add check to check if there are any fields left blank, return 500 if so
+        if (!username || !password) {
+            return res.status(400).json({message: 'Username and/or Password are missing!'})
+        }
+
+        if (user) {
+            // todo: add check to check if password is correct and return 401/403 (need check) if incorrect password
+            const validPassword = await _checkPassword(username, password);
+            if (validPassword) {
+                const token = jwt.sign({
+                    username: username
+                }, "helloworld", { expiresIn: '3h'}) // helloworld is the jwt secret key, it's just an example and should put in env file
+
+                return res.status(200).json({message: 'Authentication successful', token: token})
+            } else {
+                return res.status(401).json({message: 'Incorrect username or password!'})
+            }
+        } else {
+            return res.status(404).json({message: `Username: ${username} not found in database!`})
+        }       
     } catch (err) {
         return res.status(500).json({message: 'Database failure when retrieving a user!'})
     }
@@ -49,4 +60,28 @@ export async function userLogin(req, res) {
 
 export async function deleteUser(req, res) {
 
+}
+
+export async function changePassword(req, res) {
+    try {
+        const { username, password } = req.body;
+        const user = await _checkUser(username);
+
+        if (!username || !password) {
+            return res.status(400).json({message: 'Username and/or Password are missing!'})
+        }
+
+        if (user) {
+            const resp = await _changePassword(username, password);
+            if (resp.err) {
+                return res.status(400).json({message: 'Could not change password!'});
+            } else {
+                return res.status(200).json({message: 'Password changed successfully!'})
+            }
+        } else {
+            return res.status(404).json({message: `Username: ${username} not found in database!`})
+        }       
+    } catch (err) {
+        return res.status(500).json({message: 'Database failure when changing password!'})
+    }
 }
