800285bfd7b2031745664ec377e3f00212b9a85e
nicoleang09 <60417365+nicoleang09@users.noreply.github.com>
2022-10-27 20:03:07 +0800

    Add test file for question service

---
+++ question-service/test/test.js
@@ -0,0 +1,19 @@
+import { app } from "../index.js"
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+const assert = chai.assert;
+
+chai.use(chaiHttp)
+chai.should() // assertion lib
+
+describe('GET', () => {
+    it('should show hello world message', (done) => {
+        chai.request(app)
+            .get("/api/question")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.text.should.equal("Hello World from question-service")
+                done();
+            })
+    })
+})
